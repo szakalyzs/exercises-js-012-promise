5e30ba24e3d4e6b2340317d0817a48891139d6f0
unknown <szakalyzs@gmail.com>
2020-12-26 19:06:22 +0100

    All task done.

--- main.js
+++ main.js
@@ -2,24 +2,41 @@
 
 const array = ['apple','strawberry','lemon','orange','banana'];
 
-function transformUpperCase(array) {
+//-------------------------Task 1---------------------------------
+
+const convertToUpper = array => array.map(item => item.toUpperCase());
+
+const sortArray = array => array.sort();
+
+function transformStrings(array,calledFunction) {
     return new Promise((resolve, reject) => {
-        const isAllString = array.reduce((prev, curr) => prev && (typeof (curr) === 'string'));
+        const isAllString = array.every(item => typeof(item) === 'string');
+        console.log(isAllString);
         if(isAllString) {
-            resolve(array.map(item => item.toUpperCase()));
+            try {
+                resolve(calledFunction(array));
+            } catch {
+                console.error('Something went wrong with sorting words.');
+            }
         } else {
             reject(Error('Not all items in the array are strings!'));
         }
     })
-
-};
-
-function show(arr) {
-    const text = arr.reduce((prev, curr) => prev += `<p>${curr}</p>`);
-    document.body.insertAdjacentHTML('afterbegin', text);
+    
 }
 
-transformUpperCase(array).then(data => show(data));
-transformUpperCase(array).catch(console.log);
+function show(arr,task) {
+    document.body.insertAdjacentHTML('beforeend', `<h3>Task ${task}</h3>`);
+    const text = arr.reduce((prev, curr) => prev += `<p>${curr}</p>`);
+    document.body.insertAdjacentHTML('beforeend', text);
+}
 
-//console.log(typeof ('plum'));
+transformStrings(array,convertToUpper).then(data => show(data,1));
+//transformStrings(array).catch(console.log);
+
+//-------------------------Task 2---------------------------------
+
+
+transformStrings(array,sortArray).then(data => show(data,2));
+//transformStrings(array).catch(console.log);
+
